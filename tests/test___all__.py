2280236b7668543765d86149c6aefa05042cb524
AlexWaygood <alex.waygood@gmail.com>
2022-10-18 12:40:54 +0100

    Improvements to `test___all__`

--- tests/test___all__.py
+++ tests/test___all__.py
@@ -4,6 +4,7 @@ import builtins
 import importlib
 import pkgutil
 import types
+from itertools import chain
 from typing import Final
 
 import pytest
@@ -21,28 +22,40 @@ def test___all___alphabetisation(module: types.ModuleType) -> None:
     assert module.__all__ == sorted(module.__all__)
 
 
-def _is_from_other_module(obj: object) -> bool:
-    return getattr(obj, "__module__", "typeshed_stats") != "typeshed_stats"
-
-
 @pytest.mark.parametrize("module", ALL_MODULES)
 def test_all_public_names_in___all__(module: types.ModuleType) -> None:
     """Test that all names not in `__all__` are marked as private."""
-    assert set(module.__all__) >= {
-        name
-        for name, value in vars(module).items()
-        if not (
+
+    def is_from_other_module(obj: object) -> bool:
+        return getattr(obj, "__module__", "typeshed_stats") != "typeshed_stats"
+
+    def is_private_or_imported_symbol(name: str, value: object) -> bool:
+        return (
             name.startswith("_")
             or isinstance(value, types.ModuleType)
-            or _is_from_other_module(value)
+            or is_from_other_module(value)
             or name in vars(builtins)
+        )
+
+    public_names_not_in___all__ = {
+        name
+        for name, value in vars(module).items()
+        if (
+            name not in module.__all__
+            and not is_private_or_imported_symbol(name, value)
         )
     }
 
+    if public_names_not_in___all__:
+        pytest.fail(f"Public names not in __all__: {public_names_not_in___all__!r}")
+
 
 def test___init___imports_everything_public_from_submodules() -> None:
-    all_as_set = set(typeshed_stats.__all__)
-    submodule_alls_combined = set(sum((mod.__all__ for mod in ALL_MODULES), start=[]))
-    assert (
-        all_as_set >= submodule_alls_combined
-    ), f"Missing names: {submodule_alls_combined - all_as_set!r}"
+    submodule_alls_combined = chain.from_iterable(mod.__all__ for mod in ALL_MODULES)
+    names_not_imported_in___init__ = {
+        name for name in submodule_alls_combined if name not in typeshed_stats.__all__
+    }
+    if names_not_imported_in___init__:
+        pytest.fail(
+            f"Names not imported in __init__.py: {names_not_imported_in___init__!r}"
+        )
