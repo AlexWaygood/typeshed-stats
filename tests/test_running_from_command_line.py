1e8d2b9722a595afb87a3a3378b41981ecacc0f0
Alex Waygood <Alex.Waygood@Gmail.com>
2025-05-25 15:13:35 +0100

    Add a few more Ruff lints to CI (#336)

--- tests/test_running_from_command_line.py
+++ tests/test_running_from_command_line.py
@@ -11,9 +11,11 @@ import pytest
     "args", [[sys.executable, "-m", "typeshed_stats"], ["typeshed-stats"]]
 )
 def test_running_from_command_line(complete_typeshed: Path, args: list[str]) -> None:
-    result = subprocess.run([*args, "--typeshed-dir", str(complete_typeshed)])
+    result = subprocess.run(
+        [*args, "--typeshed-dir", str(complete_typeshed)], check=False
+    )
     code = result.returncode
     assert code == 0
-    result2 = subprocess.run([*args, "--help"])
+    result2 = subprocess.run([*args, "--help"], check=False)
     code = result2.returncode
     assert code == 0
